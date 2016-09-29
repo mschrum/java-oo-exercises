fb6c58adae4ce9ab192c281de5a6d2e4afd8681d
Melissa Schrum <melissaschrum@gmail.com>
2016-09-29 00:06:39 -0500

    Created move and rotate methods

--- java-track/exercises/Robot.java
+++ java-track/exercises/Robot.java
@@ -4,14 +4,21 @@ public class Robot {
 	private int speed;
 	private int xPosition;
 	private int yPosition;
-	private int orientation;
+	private String orientation;
 
+	public Robot(String name, int speed, int xPosition, int yPosition, String orientation){
+		this.name=name;
+		this.speed=speed;
+		this.xPosition=xPosition;
+		this.yPosition=yPosition;
+		this.orientation=orientation;
+	}
 	public Robot(String name, int speed, int xPosition, int yPosition){
 		this.name=name;
 		this.speed=speed;
 		this.xPosition=xPosition;
 		this.yPosition=yPosition;
-		this.orientation=180;
+		this.orientation="East";
 	}
 	//Getters
 	public String getName (){
@@ -26,24 +33,79 @@ public class Robot {
 	public int getyPosition(){
 		return this.yPosition;
 	}
-	public int getOrientation(){
+	public String getOrientation(){
 		return this.orientation;
 	}
 	//can move
-	//can rotate
-	public void setOrientation(int newOrientation){
+	public void move (int newSpeed) {
+		this.speed = newSpeed;
+		if (this.orientation.equals("North")||this.orientation.equals("north")) {
+			this.yPosition+=this.speed;
+		}
+		else if (this.orientation.equals("South")||this.orientation.equals("south")){
+			this.yPosition-=this.speed;
+		}
+		else if (this.orientation.equals("East")||this.orientation.equals("east")){
+			this.xPosition+=this.speed;
+		}
+		else if (this.orientation.equals("West")||this.orientation.equals("west")){
+			this.xPosition-=this.speed;
+		}
+		else{
+			return;
+		}
+	}
+	public void move () {
+		if (this.orientation.equals("North")||this.orientation.equals("north")) {
+			this.yPosition+=this.speed;
+		}
+		else if (this.orientation.equals("South")||this.orientation.equals("south")){
+			this.yPosition-=this.speed;
+		}
+		else if (this.orientation.equals("East")||this.orientation.equals("east")){
+			this.xPosition+=this.speed;
+		}
+		else if (this.orientation.equals("West")||this.orientation.equals("west")){
+			this.xPosition-=this.speed;
+		}
+		else{
+			return;
+		}
+	}
+	//can rotate sets orientation
+	public void rotate (String newOrientation){
 		this.orientation=newOrientation;
 	}
 	//calc distance from another robot
-	//distance = sqrt((x1-x2)^2+(y1-y2)^2)
+	public void distance (Robot r2) {
+		double diffX = Math.abs(r2.getxPosition() - this.xPosition);
+		double diffY = Math.abs(r2.getyPosition() - this.yPosition);
+		double dX2= Math.pow(diffX, 2);
+		double dY2= Math.pow(diffY, 2);
+		double sum = dY2 + dX2;
+		double distance = Math.round(Math.sqrt(sum)*100)/100.00;
+		System.out.println("The distance between " + this.name +"'s robot and " + r2.getName() + "'s robot is " + distance );
+	}
 	//To String
 	public String toString(){
 		return "Name: " + this.name + " Speed: " + this.speed + " Position: " + this.xPosition + "," + this.yPosition + " Orientation: " + this.orientation;
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Robot myRobot = new Robot("Melissa", 2, 0, 3);
+		Robot myRobot = new Robot("Melissa", 2, 0, 3, "north");
+		Robot yourRobot = new Robot("John", 1, 0, 0, "West");
 		System.out.println(myRobot);
+		myRobot.move(3);
+		System.out.println(myRobot);
+		myRobot.rotate("South");
+		myRobot.move(2);
+		System.out.println(myRobot);
+		myRobot.move();
+		System.out.println(myRobot);
+		myRobot.rotate("west");
+		myRobot.move(1);
+		System.out.println(myRobot);
+		System.out.println(yourRobot);
+		myRobot.distance(yourRobot);
 	}
 
 }
